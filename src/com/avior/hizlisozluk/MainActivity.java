b1d7eb694d366cb0dae885309d43bd6c40b6f4b4
Ahmet Alp Balkan <ahmet@ahmetalpbalkan.com>
2011-04-30 12:34:19 +0300

    initial commit.

---
+++ src/com/avior/hizlisozluk/MainActivity.java
@@ -0,0 +1,164 @@
+package com.avior.hizlisozluk;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+	// constants
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 0x1337c0de;
+	private static final int TR_EN_MODE = 0x7;
+	private static final int EN_TR_MODE = 0x3;
+	
+	private static final String TAG = "HizliSozluk";
+	private static final String WORD_PROPNAME = "word";
+	
+	private static final HashMap<Integer, String> languageTag = new HashMap<Integer, String>(){{
+		put(TR_EN_MODE, "tr-TR");
+		put(EN_TR_MODE, "en-US");
+	}};
+	
+	// non-static variables.
+	private View[] buttons = { null, null };
+	private static int mode = 0;
+	AlertDialog choiceDialog;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		setContentView(R.layout.main);
+
+		View btn = findViewById(R.id.entr_btn);
+		buttons[0] = btn;
+
+		btn = findViewById(R.id.tren_btn);
+		buttons[1] = btn;
+
+		checkRecognition();
+	}
+
+	public void checkRecognition() {
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		if (activities.size() != 0) {
+			for (View b : buttons)
+				b.setOnClickListener(this);
+		} else {
+			for (View b : buttons) {
+				b.setEnabled(false);
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.entr_btn) {
+			mode = EN_TR_MODE;
+			startVoiceRecognitionActivity();
+		}
+		if (v.getId() == R.id.tren_btn) {
+			mode = TR_EN_MODE;
+			startVoiceRecognitionActivity();
+		}
+	}
+
+	private void startVoiceRecognitionActivity() {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+				getString(R.string.prompt));
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, languageTag.get(mode));
+		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE
+				&& resultCode == RESULT_OK) {
+			List<String> matches = data
+					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+			
+			if (matches == null || matches.size() < 1){
+				Log.w(TAG, "Matches list is 'null' or size is 0.");
+				return;
+			} else if (matches.size() == 1){
+				Log.i(TAG, "Single result redirection.");
+				processResult(matches.get(0));
+			} else {
+				Log.d(TAG, "Showing results dialog.");
+				showResultsDialog(matches);
+			}
+		}
+
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private void showResultsDialog(List<String> results) {
+		final CharSequence[] items = new CharSequence[results.size()];
+		int offset = 0;
+		for(String s : results)
+			items[offset++] = s;
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(getString(R.string.pick));
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int item) {
+		    	// process chosen item
+		        processResult(items[item]);
+		    }
+		});
+		
+		// show results dialog
+		choiceDialog = builder.create();
+		choiceDialog.show();
+	}
+
+	protected void processResult(CharSequence result) {
+		Log.i(TAG, String.format("Requested processing for `%s`", result));
+		
+		if (choiceDialog != null) choiceDialog.hide(); // it may be left visible mistakenly. i don't know.
+		
+		Intent i = new Intent(getApplicationContext(), ResultsActivity.class);
+		i.putExtra(WORD_PROPNAME, result);
+		startActivity(i);
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// handle menu key
+		if (keyCode == KeyEvent.KEYCODE_MENU){
+			showAbout(getApplicationContext());
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private void showAbout(Context context) {
+		Dialog d = new Dialog(this);
+		d.setContentView(R.layout.about);
+		d.setTitle(getString(R.string.about_title));
+		d.setCancelable(true);
+		d.show();
+	}
+}
---
+++ src/com/avior/hizlisozluk/ResultsActivity.java
@@ -0,0 +1,103 @@
+package com.avior.hizlisozluk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class ResultsActivity extends Activity{
+	private static final String SEARCH_URL = "http://m.tureng.com/search/%s";
+	//private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/534.25 (KHTML, like Gecko) Chrome/12.0.706.0 Safari/534.25"; // if spoofing needed.
+	
+	private static final String TAG = "HizliSozluk";
+	private static final String WORD_PROPNAME = "word";
+	
+	private WebView web;
+
+	protected void showErrorToast(String message){
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		getWindow().requestFeature(Window.FEATURE_PROGRESS); 
+        getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+
+		final Activity act = this; // to be accessed inside onProgressChanged
+		
+		Bundle extras = getIntent().getExtras();
+		
+		if(extras != null){
+			final String word = extras.getString(WORD_PROPNAME);
+			
+			if (word == null || "".equals(word) || word.length() < 1){
+				Log.e(TAG, "Could not find extract 'word' in ResultsActivity extras bundle.");
+				showErrorToast(getString(R.string.err_recv_word));
+				finish();
+			} else {
+				String url = String.format(SEARCH_URL, word);
+				Log.i(TAG, String.format("Loading: %s", url));
+				
+				setTitle(String.format(getString(R.string.loading_title), word));
+				
+				web = new WebView(this);
+				//web.getSettings().setUserAgentString(USER_AGENT);
+				web.getSettings().setJavaScriptEnabled(true);
+				
+				// enable progress bar
+				web.setWebChromeClient(new WebChromeClient() {
+					public void onProgressChanged(WebView view, int progress) {
+						act.setProgress(progress * 100);
+						
+						if(progress == 100) // finished loading
+							setTitle(String.format(getString(R.string.results_title), word));
+					}
+				});
+				
+				// detect errors
+				web.setWebViewClient(new WebViewClient(){
+					@Override
+					public void onReceivedError(WebView view, int errorCode,
+							String description, String failingUrl) {
+						showErrorToast(description);
+						Log.e(TAG, String.format(getString(R.string.err_webclient), description));
+					}
+				});
+				
+				// key listeners
+				web.setOnKeyListener(new OnKeyListener() {
+					@Override
+					public boolean onKey(View v, int keyCode, KeyEvent event) {
+						// provide back functionality
+						if (keyCode == KeyEvent.KEYCODE_BACK && web.canGoBack()){
+							web.goBack();
+							return true;
+						}
+						
+						// enable search key.
+						if (keyCode == KeyEvent.KEYCODE_SEARCH){
+							//Intent i = new Intent(getApplicationContext(), MainActivity.class);
+							finish();
+						}
+						return false;
+					}
+				});
+				
+				web.loadUrl(url);
+				setContentView(web);
+			}
+		} else {
+			Log.e(TAG, getString(R.string.err_xtras_bundle));
+			showErrorToast(getString(R.string.err_xtras_bundle));
+			finish();
+		}
+	}
+}
